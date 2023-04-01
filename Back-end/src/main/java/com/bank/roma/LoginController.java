99f99837260b35a2a799f2d1c89c855b60f2e85f
MaikyOzr <ozirskiy.r@icloud.com>
2023-04-01 20:58:00 +0300

    add responseEntity

--- Back-end/src/main/java/com/bank/roma/LoginController.java
+++ Back-end/src/main/java/com/bank/roma/LoginController.java
@@ -4,8 +4,13 @@
  */
 package com.bank.roma;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,8 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Maiky
  */
 @RestController
+@RequestMapping("/login")
 public class LoginController {
-   List<User> users = new ArrayList();//спосок користувачів    
-   
-   
-}
+  @PostMapping("/data")
+  
+  public ResponseEntity<String> processData(@RequestBody String data){
+      return (ResponseEntity<String>) ResponseEntity.ok();
+  }
+   }
+  
